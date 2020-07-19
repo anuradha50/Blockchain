2b8fa502673bae969019e58bcd78b704522872af
anuradha50 <66302519+anuradha50@users.noreply.github.com>
2020-07-19 15:56:04 +0530

    Add files via upload

---
+++ Assignment3.js
@@ -0,0 +1,39 @@
+/*
+Grocery shopping
+*/
+
+/*
+let arr=['Toothpaste','Brush','flour','Oil']
+*/
+let shoppingList = [array1]
+let shoppingBasket = [shoppingList.concat([array2])]
+
+
+
+/*
+for shoppingList.items[];
+    {
+        this.skuArray = [];
+        this.SaveItem = [];
+    };
+
+    shoppingBasket.prototype.toNumber = function(value)
+   {
+       form.remove();
+   } 
+   ShoppingCart.prototype.checkoutCOD = function (parms, clearCart) {
+   }
+   function CartItem(sku, name, slug, mrp, price, quantity, image, category, packing) {
+        this.sku = sku;
+        this.name = name;
+        this.slug = slug;
+        this.image = image;
+        this.category = category;
+        this.packing = packing;
+        this.mrp = mrp;
+        this.price = price * 1;
+        this.quantity = quantity * 1;
+        this.status = 0;
+    }
+
+*/
